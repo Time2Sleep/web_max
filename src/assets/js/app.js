163392f4ecdb076fb272cb9f4200961e7aa3204b
Airat Zaripov <omg.airat@hotmail.com>
2021-03-20 14:05:50 +0300

    init

---
+++ src/assets/js/app.js
@@ -0,0 +1,23 @@
+
+function onRem() {
+    let width = document.documentElement.clientWidth;
+    switch (true) {
+        case (width > 650):
+            document.documentElement.style.fontSize = width / 1920 + 'px';
+            break;
+        case (width < 650):
+            document.documentElement.style.fontSize = width / 320 + 'px';
+            break;
+    }
+}
+
+window.addEventListener('resize', onRem);
+
+onRem();
+
+let nav = document.getElementById("header__inner");
+    
+
+function toggleNav(){
+    nav.classList.toggle("header__inner--active");
+}
